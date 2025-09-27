d9627afc3cf9c5d41bc743390a5bd335366fe75c
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2025-09-28 00:03:53 +0200

    close menu when clicked outside

--- src/components/NotificationBell/NotificationBell.tsx
+++ src/components/NotificationBell/NotificationBell.tsx
@@ -3,17 +3,19 @@ import { useTypedSelector } from '@/stores/rootReducer';
 import { appendNotifications } from '@/stores/slices/notificationSlice';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import { Sheet, Typography, Stack, Dropdown, MenuButton, Menu } from '@mui/joy';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import type { NotificationObject } from '@custom-types/notification-service';
 import NotificationBellBadge from './NotificationBellBadge/NotificationBellBadge';
+import { ClickAwayListener } from '@mui/material';
 
 const NotificationBell = () => {
   let notifications = useTypedSelector((state) => state.notifications.data);
   let dispatch = useDispatch();
   let { getNotifications } = useApi();
   let { t } = useTranslation();
+  const [open, setOpen] = useState(false);
 
   useEffect(() => {
     if (notifications.length > 0) return;
@@ -26,47 +28,49 @@ const NotificationBell = () => {
   }, []);
 
   return (
-    <Dropdown>
+    <Dropdown open={open} onOpenChange={(_, isOpen) => setOpen(isOpen)}>
       <MenuButton variant="outlined" sx={{ p: 1.3 }}>
         <NotificationBellBadge>
           <NotificationsNoneOutlinedIcon />
         </NotificationBellBadge>
       </MenuButton>
-      <Menu variant="outlined" sx={{ p: 2, width: 300 }}>
-        <Stack
-          spacing={1}
-          sx={{
-            height: '60vh',
-            overflowY: 'auto',
-            maxHeight: '60vh',
-            pr: 1,
-            boxSizing: 'border-box',
-          }}
-        >
-          <Typography
-            component="h6"
-            sx={{ fontSize: '1.125rem', fontWeight: 600 }}
+      <ClickAwayListener onClickAway={() => setOpen(false)}>
+        <Menu variant="outlined" sx={{ p: 2, width: 300 }}>
+          <Stack
+            spacing={1}
+            sx={{
+              height: '60vh',
+              overflowY: 'auto',
+              maxHeight: '60vh',
+              pr: 1,
+              boxSizing: 'border-box',
+            }}
           >
-            {t('components.notificationBell.title')}
-          </Typography>
-          {notifications.length === 0 ? (
-            <Typography sx={{ fontSize: '0.875rem' }}>
-              {t('components.notificationBell.noNotifications')}
+            <Typography
+              component="h6"
+              sx={{ fontSize: '1.125rem', fontWeight: 600 }}
+            >
+              {t('components.notificationBell.title')}
             </Typography>
-          ) : (
-            notifications.map((n: NotificationObject) => (
-              <Sheet key={n.id} sx={{ p: 1 }} variant="outlined">
-                <Typography sx={{ fontSize: '1rem', fontWeight: 500 }}>
-                  {n.title}
-                </Typography>
-                <Typography sx={{ fontSize: '0.875rem' }}>
-                  {n.shortDescription}
-                </Typography>
-              </Sheet>
-            ))
-          )}
-        </Stack>
-      </Menu>
+            {notifications.length === 0 ? (
+              <Typography sx={{ fontSize: '0.875rem' }}>
+                {t('components.notificationBell.noNotifications')}
+              </Typography>
+            ) : (
+              notifications.map((n: NotificationObject) => (
+                <Sheet key={n.id} sx={{ p: 1 }} variant="outlined">
+                  <Typography sx={{ fontSize: '1rem', fontWeight: 500 }}>
+                    {n.title}
+                  </Typography>
+                  <Typography sx={{ fontSize: '0.875rem' }}>
+                    {n.shortDescription}
+                  </Typography>
+                </Sheet>
+              ))
+            )}
+          </Stack>
+        </Menu>
+      </ClickAwayListener>
     </Dropdown>
   );
 };
